e3c160dff62aa7ee71bdfcae45cd60075fa0812d
ian <ian@gregson.me>
2023-12-05 17:12:37 +0000

    add the remote crawl test to the new crawler method

--- crawler/crawler2_test.go
+++ crawler/crawler2_test.go
@@ -78,20 +78,19 @@ func Test2LocalConcurrency16(t *testing.T) {
 	}
 }
 
-//
-// func TestRemoteConcurrency64(t *testing.T) {
-// 	c := crawler.New(64)
-// 	u, _ := url.Parse("http://crawler-test.com")
-// 	_, u = urls.ValidUrl("/", u)
-// 	c.Crawl(u)
-//
-// 	got := c.VisitedCount()
-//
-// 	if got < 20 {
-// 		t.Errorf("Expected more than 20 links visited, got %d", got)
-//
-// 		for k, v := range c.Visited {
-// 			fmt.Printf("%s %v \n", k, v)
-// 		}
-// 	}
-// }
+func Test2RemoteConcurrency64(t *testing.T) {
+	c := crawler.New(64)
+	u, _ := url.Parse("http://crawler-test.com")
+	_, u = urls.ValidUrl("/", u)
+	c.Crawl2(u)
+
+	got := c.VisitedCount()
+
+	if got < 20 {
+		t.Errorf("Expected more than 20 links visited, got %d", got)
+
+		for k, v := range c.Visited {
+			fmt.Printf("%s %v \n", k, v)
+		}
+	}
+}
